c71de7450850e84582f13f2ee35b8c391d9eb566
RamPakanayev <rampakanayev@gmail.com>
2023-02-14 21:39:40 +0200

    css

--- meta-client/src/components/PlotDetails/PopUpPlotDetails.jsx
+++ meta-client/src/components/PlotDetails/PopUpPlotDetails.jsx
@@ -1,30 +1,19 @@
 import React from 'react';
 import Modal from 'react-modal';
+import './PopUpPlotDetails.css'
 
 Modal.setAppElement('#root'); // Set the app element for react-modal
 
-const PopUpPlotDetails = ({ owner, game, price, onClose }) => {
-  const customStyles = {
-    content: {
-      top: '50%',
-      left: '50%',
-      right: 'auto',
-      bottom: 'auto',
-      marginRight: '-50%',
-      transform: 'translate(-50%, -50%)',
-      backgroundColor: 'white',
-      padding: '20px',
-      border: '1px solid gray',
-      borderRadius: '5px',
-
-    },
-  };
-
+const PopUpPlotDetails = ({ owner, game, price, onClose,x, }) => {
   return (
-    <Modal isOpen={true} onRequestClose={onClose} style={customStyles}>
-      <button onClick={onClose} style={{float: 'right'}}>X</button>
+    <Modal isOpen={true} onRequestClose={onClose}>
+      <button onClick={onClose} style={{ float: 'right' }}>
+        X
+      </button>
       <h2>Plot Details</h2>
-      <p>Owner: {owner}</p>
+      <hr/>
+      <br/>
+      <p>Owner: {owner || "None"}</p>
       <p>Game: {game || 'None'}</p>
       <p>Price: {price}</p>
     </Modal>
