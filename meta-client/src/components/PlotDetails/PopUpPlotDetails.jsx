e2f1aace4a7ec5411477e2038ca69f395e9a4a66
RamPakanayev <rampakanayev@gmail.com>
2023-02-16 14:14:27 +0200

    modal to popup

--- meta-client/src/components/PlotDetails/PopUpPlotDetails.jsx
+++ meta-client/src/components/PlotDetails/PopUpPlotDetails.jsx
@@ -1,60 +1,27 @@
 import React from 'react';
-import Modal from 'react-modal';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 import './PopUpPlotDetails.css';
-
-const modalStyles = {
-  overlay: {
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  },
-  content: {
-    backgroundColor: '#292b2d',
-    color: '#f0f0f0',
-    borderRadius: '5px',
-    padding: '20px',
-    fontSize: '16px',
-    lineHeight: '1.9',
-    maxWidth: '900px',
-    margin: 'auto',
-  },
-  h2: {
-    fontSize: '44px',
-    marginTop: '0',
-    marginBottom: '0',
-    textAlign: 'center',
-  },
-  hr: {
-    borderTop: '1px solid #f0f0f0',
-    marginTop: '10px',
-    marginBottom: '10px',
-  },
-  p: {
-    fontSize: '24px',
-    marginTop: '5px',
-    marginBottom: '5px',
-    marginLeft: '3%',
-    textAlign:'left',
-  },
-  
-};
-
-
-Modal.setAppElement('#root'); // Set the app element for react-modal
 
 const PopUpPlotDetails = ({ owner, game, price, onClose, x, y }) => {
   return (
-    <Modal isOpen={true} onRequestClose={onClose} style={modalStyles}>
-      <button onClick={onClose} style={modalStyles.button}>
+    <Popup open={true} onClose={onClose} className="popup-content">
+      <button className="close-btn" onClick={onClose}>
         X
       </button>
-      <h2 style={modalStyles.h2}>Plot Details</h2>
-      <hr style={modalStyles.hr} />
+      <h2 className="popup-title">Plot Details</h2>
+      <hr />
       <br />
-      <p style={modalStyles.p}>Owner: {owner || 'None'}</p>
-      <p style={modalStyles.p}>Game: {game || 'None'}</p>
-      <p style={modalStyles.p}>Price: {price}</p>
-      <p style={modalStyles.p}>X: {x}</p>
-      <p style={modalStyles.p}>Y: {y}</p>
-    </Modal>
+      <p className="popup-text">Owner: {owner || 'None'}</p>
+      <p className="popup-text">Game: {game || 'None'}</p>
+      <p className="popup-text">Price: {price}</p>
+      <p className="popup-text">X: {x}</p>
+      <p className="popup-text">Y: {y}</p>
+      <div className="popup-buttons">
+        <button className="popup-buy-btn">Buy</button>
+        <button className="popup-sell-btn">Sell</button>
+      </div>
+    </Popup>
   );
 };
 
