446a095ba41e3af9fd547a9cbbeeb724f8c6a467
RamPakanayev <rampakanayev@gmail.com>
2023-02-18 19:10:36 +0200

    PopUpPlotDetails
    Add PropTypes and improve code structure
    Refactor the PopUpPlotDetails component to use PropTypes to validate props and improve the code structure for better readability.
    
    Use boolean variables for user type and improve CSS classes
    Simplify the code by using boolean variables for the user type and improve CSS class names for the popup window based on the user type.
    
    Fix typos and add default props for callbacks
    Fix some typos in the render content and add default props for callback functions that are not required.

--- meta-client/src/App.jsx
+++ meta-client/src/App.jsx
@@ -3,6 +3,7 @@ import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import EntryPage from './components/EnteryPage/EnteryPage';
 import Grid20 from './components/Grid copy/Grid20';
+import Grid from './components/Grid/Grid'
 
 
 function App() {
@@ -27,7 +28,7 @@ function App() {
       {/* render the header with a callback to handle the home button click */}
       <Header onHomeClick={handleHomeClick} />
       {/* render the entry page or the grid based on the showGrid state */}
-      {showGrid ? <Grid20 userType={userType} /> : <EntryPage onUserTypeChange={handleUserTypeChange} />}
+      {showGrid ? <Grid userType={userType} /> : <EntryPage onUserTypeChange={handleUserTypeChange} />}
       {/* render the footer */}
       <Footer />
     </div>
--- meta-client/src/components/PlotDetails/PopUpPlotDetails.jsx
+++ meta-client/src/components/PlotDetails/PopUpPlotDetails.jsx
@@ -1,26 +1,45 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Popup from 'reactjs-popup';
 import './PopUpPlotDetails.css';
 
-const PopUpPlotDetails = ({ id, owner, game, price, onClose, userType, x, y, onSetPrice, onTransferOwnership, onAttachGame }) => {
+const PopUpPlotDetails = ({
+  id,
+  owner,
+  game,
+  price,
+  x,
+  y,
+  userType,
+  onClose,
+  onBuy,
+  onSell,
+  onSetPrice,
+  onTransferOwnership,
+  onAttachGame,
+}) => {
+  const isBuyer = userType === 'buyer';
+  const isSeller = userType === 'seller';
+
   return (
-    <Popup open={true} onClose={onClose} className={`popup-content ${userType === 'guest' ? 'popup-content--guest' : ''}`}>
-      <button className="close-btn" onClick={onClose}>
-        X
-      </button>
+    <Popup open={true} onClose={onClose} className={`popup-content ${isBuyer ? 'popup-content--buyer' : ''} ${isSeller ? 'popup-content--seller' : ''}`}>
+      <button className="close-btn" onClick={onClose}>X</button>
       <h2 className="popup-title">Plot Details</h2>
       <hr />
       <br />
-      <p className="popup-text">Plot's: {id}</p>
+      <p className="popup-text">Plot ID: {id}</p>
       <p className="popup-text">Owner: {owner || 'None'}</p>
       <p className="popup-text">Game: {game || 'None'}</p>
       <p className="popup-text">Price: {price}</p>
-      <p className="popup-text">X: {x}</p>
-      <p className="popup-text">Y: {y}</p>
-      {userType==='guest' ? null : (
+      <p className="popup-text">Coordinates: ({x}, {y})</p>
+      {isBuyer && (
         <div className="popup-buttons">
-          <button className="popup-buy-btn">Buy</button>
-          <button className="popup-sell-btn">Sell</button>
+          <button className="popup-buy-btn" onClick={onBuy}>Buy</button>
+        </div>
+      )}
+      {isSeller && (
+        <div className="popup-buttons">
+          <button className="popup-sell-btn" onClick={onSell}>Sell</button>
           <button className="popup-price-btn btn" onClick={onSetPrice}>Set Price</button>
           <button className="popup-transfer-btn btn" onClick={onTransferOwnership}>Transfer Ownership</button>
           <button className="popup-game-btn btn" onClick={onAttachGame}>Attach Game</button>
@@ -30,5 +49,30 @@ const PopUpPlotDetails = ({ id, owner, game, price, onClose, userType, x, y, onS
   );
 };
 
+PopUpPlotDetails.propTypes = {
+  id: PropTypes.number.isRequired,
+  owner: PropTypes.string,
+  game: PropTypes.string,
+  price: PropTypes.number.isRequired,
+  x: PropTypes.number.isRequired,
+  y: PropTypes.number.isRequired,
+  userType: PropTypes.oneOf(['guest', 'buyer', 'seller']).isRequired,
+  onClose: PropTypes.func.isRequired,
+  onBuy: PropTypes.func,
+  onSell: PropTypes.func,
+  onSetPrice: PropTypes.func,
+  onTransferOwnership: PropTypes.func,
+  onAttachGame: PropTypes.func,
+};
+
+PopUpPlotDetails.defaultProps = {
+  owner: null,
+  game: null,
+  onBuy: null,
+  onSell: null,
+  onSetPrice: null,
+  onTransferOwnership: null,
+  onAttachGame: null,
+};
 
 export default PopUpPlotDetails;
