d6fc378ab3927f4e98d7bb0293dc3ada18a03486
RamPakanayev <rampakanayev@gmail.com>
2023-02-25 18:10:58 +0200

    STYLE

--- meta-client/src/components/Header/Header.jsx
+++ meta-client/src/components/Header/Header.jsx
@@ -1,36 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 import Web3 from 'web3';
 
-
-async function metamaskConnection() {
-  try {
-    if (window.ethereum) {
-      const res = await window.ethereum.request({ method: 'eth_requestAccounts' });
-      const address = res[0];
-      console.log('Address:', address);
-      const web3 = new Web3(window.ethereum);
-      const balance = await web3.eth.getBalance(address);
-      console.log('Wallet balance:', web3.utils.fromWei(balance, 'ether'));
-      const count = await web3.eth.getTransactionCount(address);
-      console.log('Transaction count:', count);
-      const estimate = await web3.eth.estimateGas({ from: address, to: address, value: web3.utils.toWei('0.01', 'ether') });
-      console.log('Gas estimation:', estimate.toString());
-      const logs = await web3.eth.getPastLogs({ fromBlock: '0x0', address: address });
-      console.log('Logs:', logs);
-    } else {
-      const messageEl = document.createElement('div');
-      messageEl.textContent = 'Please install the Metamask extension.';
-      messageEl.style.color = 'red';
-      document.body.appendChild(messageEl);
-    }
-  } catch (error) {
-    console.error('Error connecting to Metamask:', error);
-  }
-}
-
-
 const Header = ({ onHomeClick }) => {
+  const [connected, setConnected] = useState(false);
+  const [address, setAddress] = useState(null);
+
+  const toggleConnection = async () => {
+    try {
+      if (window.ethereum) {
+        if (connected) {
+          await window.ethereum.request({ method: 'wallet_requestPermissions', params: [{ eth_accounts: {} }] });
+          setConnected(false);
+          setAddress(null);
+        } else {
+          const res = await window.ethereum.request({ method: 'eth_requestAccounts' });
+          setAddress(res[0]);
+          setConnected(true);
+          const web3 = new Web3(window.ethereum);
+          const balance = await web3.eth.getBalance(address);
+          console.log('Wallet balance:', web3.utils.fromWei(balance, 'ether'));
+          const count = await web3.eth.getTransactionCount(address);
+          console.log('Transaction count:', count);
+          const estimate = await web3.eth.estimateGas({ from: address, to: address, value: web3.utils.toWei('0.01', 'ether') });
+          console.log('Gas estimation:', estimate.toString());
+          const logs = await web3.eth.getPastLogs({ fromBlock: '0x0', address: address });
+          console.log('Logs:', logs);
+        }
+      } else {
+        const messageEl = document.createElement('div');
+        messageEl.textContent = 'Please install the Metamask extension.';
+        messageEl.style.color = 'red';
+        document.body.appendChild(messageEl);
+      }
+    } catch (error) {
+      console.error('Error connecting to Metamask:', error);
+    }
+  };
+
   return (
     <header className="header">
       <img className="logo" src="/logo.png" alt="Meta_DeCentraland Logo" />
@@ -52,7 +59,10 @@ const Header = ({ onHomeClick }) => {
           </li>
         </ul>
       </nav>
-      <button className='btn' onClick={metamaskConnection}>Connect Wallet</button>
+      {connected ?<p>Address: {address}</p>:null}
+      <button className='btn' onClick={toggleConnection}>
+      Connect Wallet
+      </button>
     </header>
   );
 };
